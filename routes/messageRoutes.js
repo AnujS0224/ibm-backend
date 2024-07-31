d7309e75f0699e6492021d2c095f5878b9d7de68
Neeraj Gupta <105665359+NeerajGupta4820@users.noreply.github.com>
2024-07-31 12:05:58 +0530

    done

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -4,7 +4,7 @@ import { sendMessage, getMessages } from '../controllers/messageController.js';
 const router = express.Router();
 
 // Message routes
-router.post('/', sendMessage);
-router.get('/', getMessages);
+router.post('/create', sendMessage);
+router.get('/get', getMessages);
 
 export default router;
